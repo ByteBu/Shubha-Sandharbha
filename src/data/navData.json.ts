d2cd74c949868058a55056bd7090fc600163fab4
Raman <purposelessraman@gmail.com>
2024-09-01 23:00:17 +0530

    Website Testing

---
+++ src/data/navData.json.ts
@@ -0,0 +1,20 @@
+import { imageConfig } from "astro:assets";
+
+export const navLinks = [
+    {
+        text: "Home",
+        href: "/",
+    },
+    {
+        text: "Services",
+        href: "/services/",
+    },
+    {
+        text: "About Us",
+        href: "/about/",
+    },
+    {
+        text: "Contact Us",
+        href: "/contactus/",
+    },
+];
